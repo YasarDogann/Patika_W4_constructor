5131b4cd37a379862315e9c4d69538660caa575c
YasarDogann <yasardgn99@gmail.com>
2024-10-16 12:33:39 +0300

    const

---
+++ patika_w4_constructor/Bebek.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_w4_constructor
+{
+    public class Bebek
+    {
+        //fields
+        private string name;
+        private string surname;
+        private DateTime birthday;
+
+        //public özellikler Properies Encapsulation
+        public string Name { get { return name; } set { name = value; } }
+        public string Surname { get { return surname; } set { surname = value; } }
+        public DateTime Birthday { get { return birthday; } set { birthday = value; } }
+
+
+        //default Constructor
+        public Bebek()
+        {
+            birthday = DateTime.Now; //Doğum tarihi şuanki zaman olarak ayarlanır
+            BabySound();  //Bebek sesi çıktısı
+        }
+        //alternatif constructor
+        public Bebek(string _name, string _surname) : this() //default constructor'ı çağırır
+        {
+            //özel alanlara değer atama (fields)
+            this.name = _name;
+            surname = _surname;
+            BabySound();
+        }
+
+        public void BabyInfo() //bebek bilgilerini ekrana yazdırma
+        {
+            Console.WriteLine($"Adı: {name}");
+            Console.WriteLine($"Soyadı: {surname}");
+            Console.WriteLine($"Doğum Tarihi: {birthday}");
+        }
+
+        public void BabySound() //bebek sesi
+        {
+            Console.WriteLine("INGAAA");
+        }
+    }
+}
---
+++ patika_w4_constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace patika_w4_constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebek baby1 = new Bebek(); //default constructor bir bebek nesnesi oluşturdu
+            baby1.Name = "Erdal";  //adı set edildi
+            baby1.Surname = "Kömürcü"; //soyadı set edildi
+            Console.WriteLine("Baby1'in Bilgileri:\r\n");
+            baby1.BabyInfo(); //baby1'in adı, soyadı ve doğum tarihini yazdırır
+
+            Console.WriteLine("----------------------------");
+
+            Bebek baby2 = new Bebek("Abuzer", "Kömürcü");  //alternatif constructor kullanılarak bebek2 nesnesi oluşturuldu
+            Console.WriteLine("Baby2'nin Bilgileri:\r\n");
+            baby2.BabyInfo(); //ekrana yazdırıldı
+        }
+    }
+}
